6d5c1a6f5f9b63684527da11545e71693d708bdd
cwanii66 <55499491+cwanii66@users.noreply.github.com>
2022-12-13 11:21:53 +0800

    chore: promise_2

--- bd/bi.js
+++ bd/bi.js
@@ -1,20 +1,83 @@
 // 1. simple promiseAll
 function promiseAll(array) {
-  return new Promise((rs, rj) => {
-    const result = []
-    let count = 0
+  const result = []
+  return new Promise((resolve, reject) => {
     if (!Array.isArray(array)) {
-      Promise.resolve(array)
-        .then(v => result.push(v))
-      return result      
+      return reject('receive wrong param')
     }
     array.forEach((value, index) => {
       Promise.resolve(value)
-        .then(v => {
-          count++
+        .then((v) => {
           result.push(v)
-          if (index === array.length - 1) rs(result)
+          if (index === array.length - 1) return resolve(result)
+        })
+        .catch((r) => {
+          reject(r)
         })
     })
   })
 }
+// test case
+const promiseGenerator = function(arrayLength) {
+  return Array(arrayLength)
+    .fill(0)
+    .map((_, index) => 
+      new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(index)
+        }, Math.random() * 1000)
+      })
+    )
+}
+const promiseArray = promiseGenerator(10)
+// promiseAll(promiseArray).then(console.log)
+
+// 2. 异步串行
+const promiseFuncGenerator = function(arrayLength) {
+  return Array(arrayLength)
+    .fill(0)
+    .map((_, index) => () =>
+      new Promise((resolve) => {
+        setTimeout(() => {
+          console.log(index)
+          resolve(index)
+        }, index * 200)
+      })
+    )
+}
+const promiseGeneratorFunc = promiseFuncGenerator(10)
+/**
+ * @param {(() => Promise<any>)[]} promiseFunc 
+ */
+const promiseChain = function(promiseFunc) {
+  // promiseFunc.reduce((prev, cur) => {
+  //   return prev.then((value) => cur())
+  // }, Promise.resolve(-1))
+  promiseFunc.reduce(async (prev, cur) => {
+    await prev
+    return await cur()
+  }, Promise.resolve(-1))
+}
+// promiseChain(promiseGeneratorFunc)
+
+// 3. 设计一个调度器，每次只执行固定数目的promise
+// https://github.com/sindresorhus/delay
+/**
+ * 
+ * @param {(() => Promise<any>)[]} promiseFunc
+ * @param {number} limit
+ */
+const lockAsyncConcurrency = function(promiseFunc, limit) {
+  if (!Array.isArray(promiseFunc)) throw new Error('invalid array')
+
+  async function run() {
+    const fn = promiseFunc.shift()
+    const res = await fn()
+    promiseFunc.length && run()
+  }
+
+  for (let i = 0; i < limit; i++) run()
+}
+
+lockAsyncConcurrency(promiseGeneratorFunc, 5)
+
